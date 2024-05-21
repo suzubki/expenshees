d8e70cf4005a61e173849d774a829f192680a339
Darwin_N19 <darwinccn25@gmail.com>
2024-05-21 11:53:12 -0500

    wip: user entity

---
+++ lib/features/auth/domain/entities/user.dart
@@ -0,0 +1,13 @@
+class User {
+  final String? id;
+  final String? email;
+  final String? name;
+  final String? pictureUrl;
+
+  User({
+    this.id,
+    this.email,
+    this.name,
+    this.pictureUrl,
+  });
+}
