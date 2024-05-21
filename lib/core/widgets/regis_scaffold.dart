5f040fc39c2baf3fc29120d8384669a067e3cc51
Darwin_N19 <darwinccn25@gmail.com>
2024-05-21 11:51:19 -0500

    added a customized scaffold

---
+++ lib/core/widgets/regis_scaffold.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class RegisScaffold extends StatelessWidget {
+  const RegisScaffold({super.key, this.body});
+
+  final Widget? body;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        toolbarHeight: 16,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 48),
+        child: body,
+      ),
+    );
+  }
+}
