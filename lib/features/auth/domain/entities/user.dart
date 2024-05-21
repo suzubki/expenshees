d9634d1e955e64871b542ddcf95d7bfa84fe6f5c
Darwin_N19 <darwinccn25@gmail.com>
2024-05-21 13:25:19 -0500

    added equatable on User model

--- lib/features/auth/domain/entities/user.dart
+++ lib/features/auth/domain/entities/user.dart
@@ -1,13 +1,18 @@
-class User {
+import 'package:equatable/equatable.dart';
+
+class User extends Equatable {
   final String? id;
   final String? email;
   final String? name;
   final String? pictureUrl;
 
-  User({
+  const User({
     this.id,
     this.email,
     this.name,
     this.pictureUrl,
   });
+
+  @override
+  List<Object?> get props => [id, email, name, pictureUrl];
 }
