17138c1ccc478b111927675162d6a58fb85add20
Darwin_N19 <darwinccn25@gmail.com>
2024-05-27 00:31:22 -0500

    added abstract datasource and added as impl and added signIn with credentials

---
+++ lib/features/auth/data/datasources/remote/auth_datasource_remote_impl.dart
@@ -0,0 +1,58 @@
+import 'package:dio/dio.dart';
+import 'package:mibloc/core/api/api_url.dart';
+import 'package:mibloc/core/errors/exception.dart';
+import 'package:mibloc/features/auth/data/models/login_model.dart';
+import 'package:mibloc/features/auth/data/models/user_model.dart';
+import 'package:mibloc/features/auth/domain/usecases/login_usecase.dart';
+
+sealed class AuthRemoteDataSource {
+  Future<AuthenticatedUserModel> signInWithEmailAndPassword(LoginParams params);
+  Future<void> signUpWithEmailAndPassword(String email, String password);
+  Future<void> signOut();
+  Future<void> isSignedIn();
+  Future<String> getUser();
+}
+
+class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
+  final Dio httpClient;
+
+  const AuthRemoteDataSourceImpl(this.httpClient);
+
+  @override
+  Future<String> getUser() {
+    // TODO: implement getUser
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> isSignedIn() {
+    // TODO: implement getUser
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<AuthenticatedUserModel> signInWithEmailAndPassword(
+      LoginParams params) async {
+    try {
+      final model = LoginModel(email: params.email, password: params.password);
+
+      final result = await httpClient.post(ApiUrl.login, data: model.toJson());
+
+      return AuthenticatedUserModel.fromJson(result.data);
+    } catch (e) {
+      throw ServerException();
+    }
+  }
+
+  @override
+  Future<void> signOut() {
+    // TODO: implement signOut
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signUpWithEmailAndPassword(String email, String password) {
+    // TODO: implement signUpWithEmailAndPassword
+    throw UnimplementedError();
+  }
+}
--- lib/features/auth/domain/datasources/auth_datasource.dart
+++
@@ -1,7 +0,0 @@
-abstract class AuthDataSource {
-  Future<void> signInWithEmailAndPassword(String email, String password);
-  Future<void> signUpWithEmailAndPassword(String email, String password);
-  Future<void> signOut();
-  Future<bool> isSignedIn();
-  Future<String> getUser();
-}
