2f55e29bdb490e46f9aab6f50b5d03b872c4c302
Darwin_N19 <darwinccn25@gmail.com>
2024-05-27 17:13:23 -0500

    added validator, keyboard type, obscure text and integration with onChange method

--- lib/core/widgets/inputs/regis_text_field.dart
+++ lib/core/widgets/inputs/regis_text_field.dart
@@ -8,20 +8,32 @@ enum RegisTextFieldValidator {
   name,
 }
 
-class RegisTextField extends StatelessWidget {
-  const RegisTextField(
-      {super.key,
-      this.onChanged,
-      required this.validatorLabel,
-      this.isRequired = true});
+class RegisTextField extends StatefulWidget {
+  const RegisTextField({
+    super.key,
+    this.onChanged,
+    required this.validatorLabel,
+    this.keyboardType = TextInputType.text,
+    this.isRequired = true,
+    this.obscureText = false,
+  });
 
   final RegisTextFieldValidator validatorLabel;
   final void Function(String)? onChanged;
   final bool isRequired;
+  final TextInputType keyboardType;
+  final bool obscureText;
 
+  @override
+  State<RegisTextField> createState() => _RegisTextFieldState();
+}
+
+class _RegisTextFieldState extends State<RegisTextField> {
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      obscureText: widget.obscureText,
+      keyboardType: TextInputType.emailAddress,
       decoration: InputDecoration(
         constraints: const BoxConstraints(minHeight: 44),
         border: OutlineInputBorder(
@@ -31,20 +43,21 @@ class RegisTextField extends StatelessWidget {
           ),
         ),
       ),
+      onChanged: widget.onChanged,
       autovalidateMode: AutovalidateMode.onUserInteraction,
       validator: (val) {
-        if (!isRequired) return null;
+        if (!widget.isRequired) return null;
 
         if (val == null || val.isEmpty) {
           return 'This field is required';
         }
 
-        if (validatorLabel == RegisTextFieldValidator.email &&
+        if (widget.validatorLabel == RegisTextFieldValidator.email &&
             !val.isEmailValid) {
           return 'This email is not valid';
         }
 
-        if (validatorLabel == RegisTextFieldValidator.password &&
+        if (widget.validatorLabel == RegisTextFieldValidator.password &&
             !val.isPasswordValid) {
           return 'Password must be at least 8 characters and probably doesn\'t contain have the correct format';
         }
