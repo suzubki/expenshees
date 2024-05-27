5a42a9e95364ec642ae06aa85a1b8432ef6cdb05
Darwin_N19 <darwinccn25@gmail.com>
2024-05-27 17:11:58 -0500

    changed api url endpoint

--- lib/core/api/api_url.dart
+++ lib/core/api/api_url.dart
@@ -1,7 +1,8 @@
 class ApiUrl {
   const ApiUrl._();
 
-  static const String baseUrl = 'http://localhost:3000';
+  // static const String baseUrl = 'http://localhost:3000';
+  static const String baseUrl = 'http://10.0.2.2:3000';
 
   static const String login = '$baseUrl/auth/login';
 }
