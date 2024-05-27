b8a7c79625ac843ae47df9bca5ae27bbf5b5f63d
Darwin_N19 <darwinccn25@gmail.com>
2024-05-27 17:12:13 -0500

    added regis observer with logger

---
+++ lib/core/utils/bloc_observer.dart
@@ -0,0 +1,28 @@
+import 'package:bloc/bloc.dart';
+import 'logger.dart';
+
+class RegisBlocObserver extends BlocObserver {
+  @override
+  void onEvent(Bloc bloc, Object? event) {
+    regisLogger.d('Event Bloc $bloc');
+
+    super.onEvent(bloc, event);
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    regisLogger.e('Bloc Error: $bloc');
+    regisLogger.e('Error log: $error');
+
+    super.onError(bloc, error, stackTrace);
+  }
+
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    regisLogger.d('Change Bloc $bloc');
+    regisLogger.d(change.currentState);
+    regisLogger.d(change.nextState);
+
+    super.onChange(bloc, change);
+  }
+}
