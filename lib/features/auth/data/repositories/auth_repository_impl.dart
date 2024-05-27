d960bbab54cdd748ae1d2b9962c2cd01634a8613
Darwin_N19 <darwinccn25@gmail.com>
2024-05-27 00:31:39 -0500

    added repository impl

---
+++ lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -0,0 +1,46 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:mibloc/core/errors/failure.dart';
+import 'package:mibloc/features/auth/data/datasources/remote/auth_datasource_remote_impl.dart';
+import 'package:mibloc/features/auth/domain/entities/__entities.dart';
+import 'package:mibloc/features/auth/domain/repositories/__repositores.dart';
+import 'package:mibloc/features/auth/domain/usecases/login_usecase.dart';
+
+class AuthRepositoryImpl implements AuthRepository {
+  final AuthRemoteDataSource _remoteDataSource;
+
+  const AuthRepositoryImpl(this._remoteDataSource);
+
+  @override
+  Future<Either<Failure, AuthenticatedUser>> signInWithEmailAndPassword(
+      LoginParams params) async {
+    try {
+      final model = LoginParams(email: params.email, password: params.password);
+
+      final result = await _remoteDataSource.signInWithEmailAndPassword(model);
+
+      return Right(result);
+    } catch (e) {
+      return Left(ServerFailure());
+    }
+  }
+
+  @override
+  Future<void> signUpWithEmailAndPassword(String email, String password) async {
+    await Future.delayed(const Duration(seconds: 3));
+  }
+
+  @override
+  Future<void> signOut() async {
+    return await Future.value();
+  }
+
+  @override
+  Future<bool> isSignedIn() async {
+    return await Future.value(true);
+  }
+
+  @override
+  Future<String> getUser() async {
+    return await Future.value('user');
+  }
+}
--- lib/features/auth/domain/repositories/auth_repository.dart
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -1,5 +1,11 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:mibloc/core/errors/failure.dart';
+import 'package:mibloc/features/auth/domain/entities/__entities.dart';
+import 'package:mibloc/features/auth/domain/usecases/login_usecase.dart';
+
 abstract class AuthRepository {
-  Future<void> signInWithEmailAndPassword(String email, String password);
+  Future<Either<Failure, AuthenticatedUser>> signInWithEmailAndPassword(
+      LoginParams params);
   Future<void> signUpWithEmailAndPassword(String email, String password);
   Future<void> signOut();
   Future<bool> isSignedIn();
