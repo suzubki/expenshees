8c6e3be2007998052307a5b9f4c1e4ecdde7a634
Darwin_N19 <darwinccn25@gmail.com>
2024-05-27 00:32:16 -0500

    added authenticated user entity

--- lib/features/auth/domain/entities/user.dart
+++ lib/features/auth/domain/entities/user.dart
@@ -1,18 +1,46 @@
 import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
 
 class User extends Equatable {
-  final String? id;
-  final String? email;
-  final String? name;
-  final String? pictureUrl;
+  final String id;
+  final String email;
+  final String name;
+
+  @JsonKey(name: 'picture_url')
+  final String pictureUrl;
+
+  @JsonKey(name: 'created_at')
+  final DateTime createdAt;
+
+  @JsonKey(name: 'updated_at')
+  final DateTime updatedAt;
 
   const User({
-    this.id,
-    this.email,
-    this.name,
-    this.pictureUrl,
+    required this.id,
+    required this.email,
+    required this.name,
+    required this.pictureUrl,
+    required this.createdAt,
+    required this.updatedAt,
   });
 
   @override
   List<Object?> get props => [id, email, name, pictureUrl];
+}
+
+class AuthenticatedUser extends Equatable {
+  final String userId;
+  final String name;
+  final String accessToken;
+  final String refreshToken;
+
+  const AuthenticatedUser({
+    required this.userId,
+    required this.name,
+    required this.accessToken,
+    required this.refreshToken,
+  });
+
+  @override
+  List<Object?> get props => [userId, name, accessToken, refreshToken];
 }
