85332d332fdcab0cd43f2510477369c610c22d75
Darwin_N19 <darwinccn25@gmail.com>
2024-05-27 00:32:05 -0500

    added login usecase

---
+++ lib/features/auth/domain/usecases/login_usecase.dart
@@ -0,0 +1,34 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:mibloc/core/errors/failure.dart';
+import 'package:mibloc/core/extensions/extensions.dart';
+import 'package:mibloc/core/usecases/usecase.dart';
+import 'package:mibloc/features/auth/domain/entities/user.dart';
+import 'package:mibloc/features/auth/domain/repositories/__repositores.dart';
+
+class LoginUseCase implements UseCase<AuthenticatedUser, LoginParams> {
+  final AuthRepository _repository;
+
+  const LoginUseCase(this._repository);
+
+  @override
+  Future<Either<Failure, AuthenticatedUser>> call(LoginParams params) async {
+    if (!params.email.isEmailValid) {
+      return Left(InvalidEmailFailure());
+    }
+
+    if (!params.password.isPasswordValid) {
+      return Left(InvalidPasswordFailure());
+    }
+
+    return await _repository.signInWithEmailAndPassword(params);
+  }
+}
+
+class LoginParams {
+  final String email;
+  final String password;
+
+  LoginParams({required this.email, required this.password});
+
+  List<Object?> get props => [email, password];
+}
