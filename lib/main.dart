cf9cec76ab6b2f9a7dee966484b14a6ab3e69ef9
Darwin_N19 <darwinccn25@gmail.com>
2024-05-27 17:16:47 -0500

    added bloc initialization and multiprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mibloc/core/configs/injector_conf.dart';
 import 'package:mibloc/core/theme.dart';
+import 'package:mibloc/core/utils/bloc_observer.dart';
+import 'package:mibloc/features/auth/presentation/bloc/auth/auth_bloc.dart';
 import 'package:mibloc/features/auth/presentation/screens/login_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(const MyApp());
+  configureDependencies();
+
+  Bloc.observer = RegisBlocObserver();
+
+  runApp(MultiBlocProvider(
+    providers: [
+      BlocProvider(
+        create: (context) => getIt<AuthBloc>(),
+      )
+    ],
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
