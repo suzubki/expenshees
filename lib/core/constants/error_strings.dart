450a4116407b8681ba1fb8a703299627348f65fa
Darwin_N19 <darwinccn25@gmail.com>
2024-05-29 23:05:47 -0500

    added class with common error messages

---
+++ lib/core/constants/error_strings.dart
@@ -0,0 +1,12 @@
+class ErrorStrings {
+  static const String noInternet = 'No internet connection';
+
+  static const String badCredentials =
+      'The email address or password you entered is incorrect. Please verify the credential and try again';
+
+  static const String serverError =
+      "We're sorry. We were unable to process your request. Please try again.";
+
+  static const String unknownError =
+      'Something went wrong. Please try again later.';
+}
