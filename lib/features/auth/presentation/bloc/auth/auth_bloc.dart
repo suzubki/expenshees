657cafd73d1eab2ebfdb7c8e6ffcaa73d560e726
Darwin_N19 <darwinccn25@gmail.com>
2024-05-29 23:07:53 -0500

    added auth failure flow

--- lib/features/auth/presentation/bloc/auth/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth/auth_bloc.dart
@@ -3,6 +3,8 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
+import 'package:mibloc/core/constants/error_strings.dart';
+import 'package:mibloc/core/errors/failure.dart';
 import 'package:mibloc/features/auth/domain/entities/__entities.dart';
 import 'package:mibloc/features/auth/domain/usecases/login_usecase.dart';
 
@@ -28,7 +30,14 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
 
     result.fold(
       (failure) {
-        emit(const AuthLoginFailureState(message: 'Login failed'));
+        if (failure is CredentialFailure) {
+          emit(const AuthLoginFailureState(
+              message: ErrorStrings.badCredentials));
+        } else if (failure is ServerFailure) {
+          emit(const AuthLoginFailureState(message: ErrorStrings.serverError));
+        } else {
+          emit(const AuthLoginFailureState(message: ErrorStrings.unknownError));
+        }
       },
       (user) {
         emit(AuthLoginSuccessState(user: user));
