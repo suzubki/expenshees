2a8d66e6e95573ea13cc155be5b6f35561a11714
Darwin_N19 <darwinccn25@gmail.com>
2024-06-08 21:26:22 -0500

    feat: added firebase app check as measure security

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,31 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mibloc/core/configs/injector_config.dart';
 import 'package:mibloc/core/theme.dart';
 import 'package:mibloc/core/utils/bloc_observer.dart';
 import 'package:mibloc/features/auth/presentation/bloc/auth/auth_bloc.dart';
+import 'package:mibloc/firebase_options.dart';
 import 'package:mibloc/routes/app_route_config.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  // Initialize Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  FirebaseAppCheck.instance.activate(
+    androidProvider: AndroidProvider.debug,
+    appleProvider: AppleProvider.debug,
+    // androidProvider: AndroidProvider.playIntegrity,
+    // appleProvider: AppleProvider.appAttest,
+  );
+
+  // Initialize the dependency injection
   configureDependencies();
 
+  // Initialize the Bloc observer, which will log all Bloc events
   Bloc.observer = RegisBlocObserver();
 
   runApp(MultiBlocProvider(
