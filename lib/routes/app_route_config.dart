12065e0676153ee87d7d63b14410c452710b00fb
Darwin_N19 <darwinccn25@gmail.com>
2024-06-08 22:52:36 -0500

    feat: added builder with login screen

--- lib/routes/app_route_config.dart
+++ lib/routes/app_route_config.dart
@@ -2,25 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mibloc/features/auth/presentation/bloc/auth/auth_bloc.dart';
+import 'package:mibloc/features/auth/presentation/screens/login_screen.dart';
 
 abstract class AppRouter {
   static GoRouter router = GoRouter(
-      initialLocation: '/login',
-      routes: [
-        GoRoute(
-          path: '/login',
-          name: 'login',
-        ),
-        GoRoute(
-          path: '/register',
-          name: 'register',
-        ),
-        GoRoute(
-          path: '/home',
-          name: 'home',
-        ),
-      ],
-      redirect: _guard);
+    initialLocation: '/login',
+    routes: [
+      GoRoute(
+        path: '/login',
+        name: 'login',
+        builder: (context, state) {
+          return const LoginScreen();
+        },
+      ),
+    ],
+    redirect: _guard,
+  );
 
   static String? _guard(BuildContext context, GoRouterState state) {
     if (context.read<AuthBloc>().state is AuthLoginSuccessState) {
