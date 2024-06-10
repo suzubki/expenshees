240d1bff2a43d7717a82645addd16cadc608d3e2
Darwin_N19 <darwinccn25@gmail.com>
2024-06-09 21:00:47 -0500

    chore: added color and border as optional properties at the button

--- lib/core/widgets/buttons/regis_filled_button.dart
+++ lib/core/widgets/buttons/regis_filled_button.dart
@@ -2,9 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:mibloc/core/theme.dart';
 
 class RegisFilledButton extends StatelessWidget {
-  const RegisFilledButton(
-      {super.key, required this.onPressed, required this.child});
+  const RegisFilledButton({
+    super.key,
+    this.color,
+    this.hasBorder = false,
+    required this.onPressed,
+    required this.child,
+  });
 
+  final bool hasBorder;
+  final Color? color;
   final Function() onPressed;
   final Widget child;
 
@@ -12,6 +19,11 @@ class RegisFilledButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return FilledButton(
       style: ButtonStyle(
+        side: hasBorder
+            ? MaterialStateProperty.resolveWith(
+                (states) => const BorderSide(color: Colors.black, width: 0.5),
+              )
+            : null,
         shape: MaterialStatePropertyAll(
           RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(8),
@@ -20,6 +32,14 @@ class RegisFilledButton extends StatelessWidget {
         minimumSize: MaterialStateProperty.resolveWith(
             (states) => const Size(double.infinity, 44)),
         backgroundColor: MaterialStateProperty.resolveWith((states) {
+          if (color != null) {
+            if (states.contains(MaterialState.hovered)) {
+              return color!.withOpacity(0.8);
+            } else {
+              return color!;
+            }
+          }
+
           if (states.contains(MaterialState.hovered)) {
             return RegisTheme.primary[400].withOpacity(0.8);
           } else {
