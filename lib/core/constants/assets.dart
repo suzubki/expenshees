514271e12ee99131c17d1abd4df3cd125a8aa8dd
Darwin_N19 <darwinccn25@gmail.com>
2024-06-09 20:59:31 -0500

    chore: added fluter_svg pckg

---
+++ lib/core/constants/assets.dart
@@ -0,0 +1,6 @@
+abstract class IconsAssets {
+  static const String _path = 'assets/icons';
+
+  static const String google = '$_path/google_icon.svg';
+  static const String apple = '$_path/apple_icon.svg';
+}
